d782758e4c05fd1115a8ca51d6ddea9272dc2d57
Min Ho Jang <128145017+minho00123@users.noreply.github.com>
2023-04-30 14:11:30 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.py
@@ -0,0 +1,13 @@
+N = int(input())
+at = '@'
+b = ' '
+for i in range(N):
+  print(at * N + b * N * 3 + at * N)
+for i in range(N):
+  print(at * N + at * N * 3 + at * N)
+for i in range(N):
+  print(at * N + b * N * 3 + at * N)
+for i in range(N):
+  print(at * N + at * N * 3 + at * N)
+for i in range(N):
+  print(at * N + b * N * 3 + at * N)
