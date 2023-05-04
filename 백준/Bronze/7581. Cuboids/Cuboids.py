57ec20dcef4d6a5d27ddffc4f82fd1a6ea1a0876
Min Ho Jang <128145017+minho00123@users.noreply.github.com>
2023-05-04 11:20:04 +0900

    [Bronze III] Title: Cuboids, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/7581. Cuboids/Cuboids.py
@@ -0,0 +1,13 @@
+while True:
+  l, w, h, v = map(int, input().split())
+  if l == 0 and w == 0 and h == 0 and v == 0:
+    break;
+  if l == 0:
+    l = v // (w * h)
+  elif w == 0:
+    w = v // (l * h)
+  elif h == 0:
+    h = v // (l * w)
+  elif v == 0:
+    v = l * w * h
+  print(l, w, h, v)
