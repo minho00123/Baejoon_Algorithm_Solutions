44c8f2fed10d53fcc50bdfa8b911459f323baca8
Min Ho Jang <128145017+minho00123@users.noreply.github.com>
2023-05-06 16:42:22 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,11 @@
+N = int(input())
+for i in range(1, N + 1):
+  print('*' * i, end='')
+  print(' ' * (N - i), end='')
+  print(' ' * (N - i), end='')
+  print('*' * i)
+for i in range(N - 1, 0, -1):
+  print('*' * i, end='')
+  print(' ' * (N - i), end='')
+  print(' ' * (N - i), end='')
+  print('*' * i)
