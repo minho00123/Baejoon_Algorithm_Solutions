73594a735426d40ad8ce6ceb045fa909dbaeb4fc
Min Ho Jang <128145017+minho00123@users.noreply.github.com>
2023-06-13 08:31:11 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅂ, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/23808. 골뱅이 찍기 － ㅂ/골뱅이 찍기 － ㅂ.py
@@ -0,0 +1,9 @@
+N = int(input())
+for _ in range(N * 2):
+  print('@' * N + ' ' * (N * 3) + '@' * N)
+for _ in range(N):
+  print('@' * (N * 5))
+for _ in range(N):
+  print('@' * N + ' ' * (N * 3) + '@' * N)
+for _ in range(N):
+  print('@' * (N * 5))
