4c6f74b19fad9cc0011e6f13370f338ce2787f90
Min Ho Jang <128145017+minho00123@users.noreply.github.com>
2023-06-21 18:19:16 +0900

    [level 0] Title: 모음 제거, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,16 @@
+def solution(my_string):
+    answer = ''
+    for i in range(len(my_string)):
+        if my_string[i] == 'a':
+            answer += ''
+        elif my_string[i] == 'e':
+            answer += ''
+        elif my_string[i] == 'i':
+            answer += ''
+        elif my_string[i] == 'o':
+            answer += ''
+        elif my_string[i] == 'u':
+            answer += ''
+        else:
+            answer += my_string[i]
+    return answer
