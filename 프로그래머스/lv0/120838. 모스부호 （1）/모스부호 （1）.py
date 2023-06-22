f85c90b70516dd3aef2a0d69bba7d6d839537cf4
Min Ho Jang <128145017+minho00123@users.noreply.github.com>
2023-06-22 20:36:15 +0900

    [level 0] Title: 모스부호 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -0,0 +1,14 @@
+def solution(letter):
+    morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+    letter_list = list(letter.split())
+    answer = ''
+    for i in range(len(letter_list)):
+        answer += morse[letter_list[i]]
+        
+    return answer
