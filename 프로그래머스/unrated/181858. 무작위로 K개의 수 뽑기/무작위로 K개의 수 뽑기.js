69844f49f718e97f13f06db11703ff13e7dc9a24
Min Ho Jang <128145017+minho00123@users.noreply.github.com>
2023-08-21 10:37:47 +0900

    [unrated] Title: 무작위로 K개의 수 뽑기, Time: 0.84 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -1,17 +1,12 @@
 function solution(arr, k) {
     const answer = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (answer.length === k) {
-            return answer;
-        }
-        
-        if (answer.indexOf(arr[i]) === -1) {
+    
+    for (let i = 0; answer.length !== k; i++) {
+        if (i >= arr.length - 1) {
+            answer.push(-1);
+        } else if (arr[i] !== answer[answer.length-1]) {
             answer.push(arr[i]);
         }
-    }
-    
-    for (let i = answer.length; i < k; i++) {
-        answer.push(-1);
     }
     
     return answer;
