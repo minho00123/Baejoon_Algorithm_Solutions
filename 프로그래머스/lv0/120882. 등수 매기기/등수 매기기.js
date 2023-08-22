4e4bbc9d3dbab260f731e842ee6a9f0bdc030109
Min Ho Jang <128145017+minho00123@users.noreply.github.com>
2023-08-22 12:46:39 +0900

    [level 0] Title: 등수 매기기, Time: 3.31 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,34 @@
+function solution(score) {
+    const average = [];
+    const sortedAverage = [];
+    for (let i = 0; i < score.length; i++) {
+        average.push((score[i][0] + score[i][1]) / 2)
+        sortedAverage.push((score[i][0] + score[i][1]) / 2)
+    }
+    sortedAverage.sort(function(a, b)  {
+        return b - a;
+    });;
+    const set = new Set(sortedAverage);
+    const setAverage = [...set]
+    
+    console.log(average);
+    let rank = 1;
+    const answer = [];
+    for (let i = 0; i < setAverage.length; i++) {
+        let count = 0;
+        for (let j = 0; j < average.length; j++) {
+            if (setAverage[i] === average[j]) {
+                answer[j] = rank;
+                count++;
+            }
+        }
+        if (count === 1) {
+            rank++;
+        } else {
+            rank += count;
+        }
+        
+    }
+    
+    return answer;
+}
